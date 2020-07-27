571322f530b33c14c5877c065be9172ac5ad7c94
Steved1mac <steved1mac@gmail.com>
2020-07-27 17:32:32 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,19 @@
   class Dog
- 
-   def bark
-    puts "Woof!"
-   end 
 
-   def sit
-   puts "The Dog is sitting" 
-   end
- end
+    def initalize(name, breed = "Mutt")
+      @name = name
+      @breed = breed
+    end 
+ 
+    def bark
+     puts "Woof!"
+    end 
+
+    def sit
+     puts "The Dog is sitting" 
+    end
+ 
 
  
- 
+   
+  end
--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,24 @@
 class Person
 
- def talk 
-  puts "Hello World!"
- end
+  
+       
+    
+  
+
+  def talk 
+      puts "Hello World!"
+  end
 
   def walk
-  puts "The Person is walking"
- end
+      puts "The Person is walking"
+      
+
+      
+  end
+  
+  
 end 
+
 
 
 
